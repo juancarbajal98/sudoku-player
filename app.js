12eff826c5345ec09cb7b84840a7ff0a88d92c55
juancarbajal98 <juancarbajal98@gmail.com>
2022-05-11 16:10:57 -0700

    initial commit - basic server

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express')
+const app = express()
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.use(express.json())
+app.use(express.static('public'))
+
+app.set('views', __dirname + '/public/views/');
+app.engine('html', require('ejs').renderFile); 
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
