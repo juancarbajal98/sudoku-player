f3de3df864448197d1664d212b411aa11adb096c
Juan Carbajal <juancarbajal98@gmail.com>
2022-05-19 19:36:47 -0700

    more recursive additions to pair solver

--- public/scripts/solver.js
+++ public/scripts/solver.js
@@ -90,6 +90,8 @@ function updateCandidates(blanks, vals, num_of_blanks){
         */
         console.log('PUZZLE NOT SOLVABLE - RETURNING WHAT WAS FOUND')
         console.log(blanks)
+        console.log(Object.keys(blanks).length)
+
         return true
     }
     // in the case that blanks still exist after we just removed a value, recursive call
@@ -98,7 +100,7 @@ function updateCandidates(blanks, vals, num_of_blanks){
 
 // TODO: Figure how to get these params provided from brute force calling (maybe global variables, maybe creating event listener when first button is clicked)
 function solver_eliminatePairs(vals, blanks){
-    // // capture submitted puzzle
+    // // capture submitted puzzle - ideally wont have to do once params issue is solved
     // const formData = new FormData(document.querySelector('form'))
 
     // set solved to false
@@ -149,14 +151,13 @@ function deduceFromPairs(blanks, vals, visited_pairs = []){
         // if current blank is not two index, we continue
         if(blanks[index].length !== 2) continue
 
-
         // mark down this new pair we are visiting
         visited_pairs.push(index)
 
 
         // not sure what params are exactly required for two funcs below
         // if change in blanks length is detected after any 3 of these then trigger recursive call
-        scanRow(index, vals, blanks)
+        scanRow(index, vals, blanks, visited_pairs)
         if(temp_length !== Object.keys(blanks).length) return deduceFromPairs(blanks, vals, visited_pairs)
         // scanCol(index, vals, blanks)
         // if(temp_length !== Object.keys(blanks).length) return deduceFromPairs(blanks, vals, visited_pairs)
@@ -165,10 +166,11 @@ function deduceFromPairs(blanks, vals, visited_pairs = []){
     }
     console.log('PROGRESS FROM OBSERVING PAIRS (in rows for now)')
     console.log(blanks)
+    console.log(Object.keys(blanks).length)
     return true
 }
 
-function scanRow(index, vals, blanks){
+function scanRow(index, vals, blanks, visited_pairs){
     // iterate over all other blanks in the same row as index
     // if candidate array differs but one or two nums appears, store a temp version with it removed
     // if none have the same candidate array as index return
@@ -179,16 +181,14 @@ function scanRow(index, vals, blanks){
     let temp = {}
     // iterate over row
     for(let i =start; i<start+9;i++){
-        // skip self
-        if(i===index) continue
-        // if this space is not blank continue
-        if(blanks[i] === undefined) continue
-        // mark pair true if pair found
-        if(blanks[i][0] === blanks[index][0] && blanks[i][1] === blanks[index][1]) {
-            pair = true
-            continue
-        }
-        // if either of the index values are found, remove from temp[0]
+        // skip self or if this space is not blank continue
+        if(blanks[i] === undefined || String(i)===index) continue
+        /* 
+        Once we've reduced that given i is not index and is indeed a blank, we can store a temporary
+        version of its candidates that has any instances of the pair values removed. If we end up
+        finding a pair we use those values to sub into blanks, otherwise we delete the temp vals array
+        */
+        // if either or both of the index values are found, remove from temp_vals
         let temp_vals = [...blanks[i]]
         let temp_length =  blanks[i].length
         let has_first = temp_vals.indexOf(blanks[index][0])
@@ -197,6 +197,18 @@ function scanRow(index, vals, blanks){
         if(has_second !== -1){temp_vals.splice(has_second,1)}
         // if at least one was found, add to temp
         if(temp_vals.length !== temp_length) temp[`${i}`] = temp_vals
+
+        // if this space does not have exactly two indices continue
+        if(blanks[i].length !== 2) continue
+        // if indices don't exactly match continue
+        if(blanks[i][0] !== blanks[index][0] || blanks[i][1] !== blanks[index][1]) continue
+
+        // at this one point we know we've found a pair
+        pair = true
+        // add to list of visited pairs
+        visited_pairs.push(String(i))
+
+        delete temp[`${i}`]
     }
 
     /* 
@@ -210,6 +222,8 @@ function scanRow(index, vals, blanks){
             if(blanks[index].length === 1){
                 vals[index] = blanks[index][0]
                 delete blanks[index]
+                // helper function to remove any instances of the newly added val in its row, col, box
+                updateBoard(index, vals, blanks, visited_pairs)
                 if(Object.keys(blanks).length === 0) return true
             }
         }
@@ -217,16 +231,91 @@ function scanRow(index, vals, blanks){
     return true
 }
 
-function furtherReduce(blanks, vals){
-    // check rows for pairs- if found remove any other appearances of pair values in row
-
-    // check cols for pairs- if found remove any other appearances of pair values in col
-
-    // check boxes for pairs- if found remove any other appearances of pair values in box
-
-}
-
 // TODO: move these last functions into a separate file for helper funcs then import here
+
+function updateBoard(new_value_index, vals, blanks, visited_pairs, new_vals=[new_value_index]){
+    /* 
+    - given that new value has just been added, we must scan its row, col, box for any redundancies.
+    - any redundancies must be removed.
+    - removed redundancies resulting in candidate arrays of length 1 will have their indices temporarily 
+      stored for recursive calls at the end of the loop
+    */
+   let checked_vals = []
+
+    // row
+    let offset = new_value_index%9
+    let start = new_value_index - offset
+    for(let i=start; i<start+9;i++){
+        // this skips all written in values, including the new index 
+        if(blanks[i] === undefined) continue
+        // if this index is a pair we visited we do not need to consider a third value as a possibility for this index representing a pair
+        if(visited_pairs.indexOf(String(i)) !== -1) continue
+        // at this point it is a value we are going to check
+        checked_vals.push(i)
+        // if the new value does not exist in the list of candidates for this index continue
+        if(blanks[i].indexOf(vals[new_value_index]) === -1) continue
+
+        // at this point the current index has a candidate array that needs to be updated
+        blanks[i].splice(blanks[i].indexOf(vals[new_value_index]), 1)
+
+        // if its length is now one we store for recursive call as this is now new info
+        if(blanks[i].length === 1) new_vals.push(i)
+    }
+
+    // col
+    for(i=0; i<9;i++){
+        // this skips all written in values, including the new index 
+        if(blanks[9*i + offset] === undefined) continue
+        // if this index is a pair we visited we do not need to consider a third value as a possibility for this index representing a pair
+        if(visited_pairs.indexOf(String(9*i + offset)) !== -1) continue
+        // at this point it is a value we are going to check
+        checked_vals.push(9*i + offset)
+        // if the new value does not exist in the list of candidates for this index continue
+        if(blanks[9*i + offset].indexOf(vals[new_value_index]) === -1) continue
+
+        // at this point the current index has a candidate array that needs to be updated
+        blanks[9*i + offset].splice(blanks[9*i + offset].indexOf(vals[new_value_index]), 1)
+
+        // if its length is now one we store for recursive call as this is now new info
+        if(blanks[9*i + offset].length === 1) new_vals.push(9*i + offset)
+    }
+
+    // box - skip all row and col values you've already checked
+    let row = Math.floor(new_value_index/9)
+    let boxNum = getBoxNum(row, offset)
+    start = Math.floor(boxNum/3)*2
+    for(let i =start; i<start+3;i++){
+        for(let j=0; j< 3;j++){
+            // this skips all written in values, including the new index 
+            if(blanks[(boxNum*3) + (i*9) + j] === undefined) continue
+            // if this index is a pair we visited we do not need to consider a third value as a possibility for this index representing a pair
+            if(visited_pairs.indexOf(String((boxNum*3) + (i*9) + j)) !== -1) continue
+            // skip all row and col values in box we've already checked
+            if(checked_vals.indexOf((boxNum*3) + (i*9) + j) !== -1) continue
+
+            checked_vals.push((boxNum*3) + (i*9) + j)
+            // if the new value does not exist in the list of candidates for this index continue
+            if(blanks[(boxNum*3) + (i*9) + j].indexOf(vals[new_value_index]) === -1) continue
+    
+            // at this point the current index has a candidate array that needs to be updated
+            blanks[(boxNum*3) + (i*9) + j].splice(blanks[(boxNum*3) + (i*9) + j].indexOf(vals[new_value_index]), 1)
+    
+            // if its length is now one we store for recursive call as this is now new info
+            if(blanks[(boxNum*3) + (i*9) + j].length === 1) new_vals.push((boxNum*3) + (i*9) + j)
+        }
+    }
+
+    // remove first index from new_vals once it has been checked for in its row, col, box (which is the code above)
+    new_vals.shift()
+    if(new_vals.length !== 0){
+        // update vals for upcoming element in new vals
+        vals[new_vals[0]] = blanks[new_vals[0]][0]
+        delete blanks[new_vals[0]]
+        // recursive call
+        return updateBoard(new_vals[0], vals, blanks, visited_pairs, new_vals)
+    }
+    return true
+}
 
 // given an index and its current candidates, iterate through its row to remove values
 function checkRow(index, vals, candidates){
