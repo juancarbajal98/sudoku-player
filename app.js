56343d46d0df06724cd65fe3b53eb871522efcd0
juancarbajal98 <juancarbajal98@gmail.com>
2022-06-04 15:50:24 -0700

    HTML data logging with axios and cheerio. Also added nodemon

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// configuration 
 app.use(express.json())
 app.use(express.static('public'))
 
@@ -14,6 +17,24 @@ app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+// methods
+async function puzzleScraper(){
+  const url = 'https://www.nytimes.com/puzzles/sudoku/easy';
+  await axios(url).then((response) => {
+    const html_data = response.data;
+    console.log('DATA: ', html_data )
+    const $ = cheerio.load(html_data);
+  });
+  const selectedElem = '#pz-game-root > div.su-app > div > div.su-app__play > div > div > div';
+
+  $(selectedElem).each((parentIndex, parentElem) => {
+    console.log()
+  })
+  return true
+}
+
+
+// endpoints
 app.get('/', (req, res) => {
   res.render('index.html')
 })
@@ -22,6 +43,16 @@ app.get('/solution-checker', (req, res) => {
   res.render('solutionChecker.html')
 })
 
-app.get('/nyt', (req, res) => {
-  res.render('nyt.html')
+app.get('/nyt', async (req, res) => {
+  try{
+    const puzzle = await puzzleScraper()
+    res.render('nyt.html')
+    return res.status(200).json({
+      result:puzzle
+    });
+  } catch (err) {
+    return res.status(500).json({
+      err: err.toString()
+    })
+  }
 })
