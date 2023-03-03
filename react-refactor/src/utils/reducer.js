721420ecd0d563cd3e4d45a892ba602fad510b4b
Juan Carbajal <juancarbajal98@gmail.com>
2023-03-02 20:35:29 -0800

    :construction: stuck on CORS issue with fetching puzzle data on react refactor

--- react-refactor/src/utils/reducer.js
+++ react-refactor/src/utils/reducer.js
@@ -3,12 +3,21 @@ const defaultSettings = {
 }
 
 const defaultState ={
-    settings: defaultSettings
+    settings: defaultSettings,
+    puzzleData: null
 }
 
 const reducer = (state, action) => {
     console.log(`Reducer triggered for action: ${JSON.stringify(action)}`)
-    return {}
+    switch(action.type){
+      case 'setPuzzleData': 
+        return {
+          ...state,
+          puzzleData: action.puzzleData
+        }
+      default:
+        throw new Error()
+    }
 }
 
 export {defaultState, reducer}
